2505cdb6e3bc652ba5225ca0f6488e0ba830423a
P1pp3n <macraybilly66@gmail.com>
2025-01-04 01:03:49 +0300

    All set

--- recipe-finder/src/Components/RecipeDetails.jsx
+++ recipe-finder/src/Components/RecipeDetails.jsx
@@ -114,7 +114,7 @@ const RecipeDetails = () => {
         onClick={addToFavorites}
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
-        Add to Favorites
+        Add to Favorites 
       </button>
     </div>
   );
