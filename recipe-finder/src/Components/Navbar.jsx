3588abd5b8955d0c24531c0b510e20a3eed9b210
P1pp3n <macraybilly66@gmail.com>
2025-01-03 22:01:26 +0300

    Updates

--- recipe-finder/src/Components/Navbar.jsx
+++ recipe-finder/src/Components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white600 text-grey p-4 shadow-md">
+    <nav className="bg-white/80 backdrop-blur-md text-grey p-4 shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="text-2xl font-bold hover:text-blue-400">
-         🍜 Recipe Finder
+          🍜 Recipe Finder
         </Link>
         {/* Navigation Links */}
         <div className="space-x-4">
